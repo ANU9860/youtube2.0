9326bec8b5125ed49e579e186c24cae06170bc5a
Anushka Singh <anushkasingh.mail333@gmail.com>
2024-02-02 19:10:14 +0530

    Add existing files

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Stack } from "@mui/material";
 
 import { categories } from "../utils/constants";
-const selectedCategory='New';
 
-const SideBar = () => (
+
+const SideBar = ({ selectedCategory,setSelectedCategory }) => (
     <Stack
     direction="row"
     sx={{
@@ -15,7 +15,7 @@ const SideBar = () => (
         {categories.map((category) =>(
             <button
             className="category-btn"
-            
+            onClick={() => setSelectedCategory(category.name)}
             style={{
               background: category.name === selectedCategory && "#FC1503",
               color: "white",
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Stack, Box } from "@mui/material";
+import { ChannelCard, Loader, VideoCard } from "./";
 
-const Videos = () => {
-  return (
-    <div>Videos</div>
-  )
+const Videos = ({ videos }) => {
+ return (
+  <Stack direction={"row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+  {videos.map((item, idx) => (
+    <Box key={idx}>
+      {item.id.videoId && <VideoCard video={item} /> }
+      {item.id.channelId && <ChannelCard channelDetail={item} />}
+    </Box>
+  ))}
+</Stack>
+ );
 }
 
 export default Videos
